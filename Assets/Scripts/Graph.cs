375af8c7f13eddfdce676116e91a7ee3130a64e3
Michaela <michaela.j.kane@gmail.com>
2021-03-25 18:27:51 +0900

    completed Building a Graph tutorial

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// practice from Catlike Coding Unity Tutorial: 
+// https://catlikecoding.com/unity/tutorials/basics/
+
+public class Graph : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform pointPrefab;
+
+    [SerializeField, Range(10,100)] // enforces range on below variable
+    private int resolution = 10;
+
+    private Transform[] points;
+
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        var position = Vector3.zero;
+
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.SetParent(transform);
+
+            position.x = (i + 0.5f) * step - 1f;
+
+            point.localPosition = position;
+            point.localScale = scale;
+
+            points[i] = point;
+        }
+    }
+
+    private void Update()
+    {
+        float time = Time.time;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+
+            point.localPosition = position;
+        }
+    }
+}
---
+++ Assets/Shaders/Point Surface.shader
@@ -0,0 +1,34 @@
+﻿Shader "Custom/Point Surface" {
+	
+	Properties {
+		_Smoothness ("Smoothness", Range(0,1)) = 0.5
+	}
+	
+	SubShader{
+
+	CGPROGRAM
+	
+	// compiler directive; instructs shader compiler to generate a surface shader with standard lighting and full support for shadows
+	#pragma surface ConfigureSurface Standard fullforwardshadows  
+	// minimum for shader's target level and quality
+	#pragma target 3.0
+
+	// define input structure for configuration function
+	struct Input {
+		float3 worldPos;
+	};
+
+	float _Smoothness;
+
+	// define ConfigureSurface method - result indicated by inout
+	void ConfigureSurface (Input input, inout SurfaceOutputStandard surface) {
+		surface.Albedo.rg = saturate(input.worldPos.xy * 0.5 + 0.5); // saturate clamps components to [0, 1]
+		surface.Smoothness = _Smoothness;
+	}
+
+	ENDCG
+	
+	}
+
+	FallBack "Diffuse"
+}
